f91e7b696d3503d9ab60229ed457d1a60a5bf423
BlueLynxUs <bluelynx.us@gmail.com>
2021-05-09 21:24:52 +0700

    upd

--- first/main.go
+++ first/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello, how are you today?")
 	fmt.Println("Welcome to Golang World")
 	fmt.Println("Welcome to Learn a Power of Go!")
 
